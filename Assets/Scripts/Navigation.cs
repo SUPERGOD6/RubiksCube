774b18b05286ada42c16aed761efec75701801d8
SUPERGOD6 <s216298792@mandela.ac.za>
2018-10-25 13:16:57 +0200

    UI WIP

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Navigation : MonoBehaviour {
+
+  public static bool fromMenu;
+
+  public void MenuToPlay()
+  {
+
+    SceneManager.LoadScene("GameScene",LoadSceneMode.Single);
+  }
+  public void PlayToMenu()
+  {
+
+    SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+  }
+  public void MenuToOptions()
+  {
+    fromMenu = true;
+    SceneManager.LoadScene("OptionMenu",LoadSceneMode.Single);
+  }
+  public void PlayToOptions()
+  {
+    fromMenu = false;
+    SceneManager.LoadScene("OptionMenu",LoadSceneMode.Single);
+  }               
+  public void OptionsBack()
+  {  
+    if (fromMenu)
+      {
+        OptionsToMenu();
+      }
+    else
+      {
+        OptionsToPlay();
+      }
+  }
+  public void OptionsToMenu()
+  {
+
+    SceneManager.LoadScene("MainMenu",LoadSceneMode.Single);
+  }
+  public void OptionsToPlay()
+  {
+
+    SceneManager.LoadScene("GameScene",LoadSceneMode.Single);
+  }
+	public void QuitGame()
+  {
+    #if UNITY_EDITOR
+      UnityEditor.EditorApplication.isPlaying = false;
+    #else
+      Application.Quit();
+    #endif
+  }
+}
---
+++ Assets/Scripts/Wait.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait : MonoBehaviour {
+                                
+	public IEnumerator Start()
+  {
+    yield return StartCoroutine(wait());
+
+  }
+  
+  public IEnumerator wait()
+  {
+    yield return new WaitForSecondsRealtime(0.1f);
+  }
+  
+}
